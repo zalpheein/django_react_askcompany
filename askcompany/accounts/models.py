22b75907710cca626f6cd0665d97a97be6aa948f
zalphee <zlaphee@gmail.com>
2021-10-29 17:37:01 +0900

    account앱에 profile 모델 추가.... 디비 생성까지

--- askcompany/accounts/models.py
+++ askcompany/accounts/models.py
@@ -1,3 +1,31 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    address = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=6)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
