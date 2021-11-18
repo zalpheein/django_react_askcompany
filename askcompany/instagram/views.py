ed04a7946484d7c4e4b31c2e6a5245ad81f9acb5
zalphee <zlaphee@gmail.com>
2021-11-18 22:16:55 +0900

    FBV --> CBV 로 전환

--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -1,6 +1,7 @@
 from django.http.request import HttpRequest
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.http.response import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 from .models import Post
 
 
@@ -18,17 +19,19 @@ def post_list(request):
  
 
 # 파이썬 3.6부터 지원하는 타입힌트 기능 예시
-def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    try:
-        post = Post.objects.get(pk=pk)
-    except:
-        raise Http404
+# FBV 예시 --> CBV 예시는 다음 항목에...
+# def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     # try:
+#     #     post = Post.objects.get(pk=pk)
+#     # except:
+#     #     raise Http404
+#     post = Post.get_object_or_404(pk=pk)
 
-    return render(request, 'instagram/post_detail.html', {
-        'post': post
-    })
+#     return render(request, 'instagram/post_detail.html', {
+#         'post': post
+#     })
 
-
+post_detail = DetailView.as_view(model=Post)
 
 
 
