d5a9a85118e33a8c605e402b5e928161b9ad6517
zalphee <zlaphee@gmail.com>
2021-11-20 23:07:12 +0900

    게시글을 추가 했고... post_list를 CBV로 변경

--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -1,22 +1,29 @@
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
 
-    if q:
-        qs = qs.filter(message__icontains=q)
+#     if q:
+#         qs = qs.filter(message__icontains=q)
 
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q': q,
-    })
- 
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q': q,
+#     })
+
+post_list = ListView.as_view(model=Post)
+
+
+
+
+
+
 
 # 파이썬 3.6부터 지원하는 타입힌트 기능 예시
 # FBV 예시 --> CBV 예시는 다음 항목에...
@@ -41,6 +48,7 @@ post_detail = DetailView.as_view(model=Post)
 #   그런데.... 만약, 로그인 허용된 사람들에게만 보여 주려고 할 경우엔?
 #   또는.... 만약, 공개 허용된 것만 보여 주려고 할 경우엔?
 #   이럴때는, 별도로 클래스를 만들고...get_queryset() 함수를 별도로 구현 해야 한다....
+
 # class PostDetailView(DetailView):
 #     model = Post
 
@@ -54,6 +62,12 @@ post_detail = DetailView.as_view(model=Post)
 #             qs = qs.filter(is_public=True)
 #         return qs
 
+# 만약, 템플릿(즉, html 파일)에 추가 내용을 전달 하려고 하면….
+#   get_context_data() 함수를 사용...
+#    템플릿(즉, html 파일)에서 사용할 내용들을 사전(dictionary) 형태로 미리 준비 하는 함수…. 
+#    이 함수에 사전 형태도 정의 해두면…템플릿 즉 html 파일에서 사용 가능
+#       def get_context_data(self):
+#           pass
 
 
 
