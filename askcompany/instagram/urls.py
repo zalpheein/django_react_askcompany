4b43ea0ffd44e9221752687cf30e0cfd1b49a622
zalphee <zlaphee@gmail.com>
2021-11-30 22:56:23 +0900

    urls.py 에 수정 관련 path 정의

--- askcompany/instagram/urls.py
+++ askcompany/instagram/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('new/', views.post_new, name='post_new'),
     path('', views.post_list, name='post_list'),
     path('<int:pk>/', views.post_detail, name='post_detail'),
-
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
 
 ]
