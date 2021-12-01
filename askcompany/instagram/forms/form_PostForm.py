abdd332dc124633d63425780c6334222976e66cb
xalphee <zalphee@gmail.com>
2021-12-01 14:35:31 +0900

    is_valid() 실행 시 자동 수행 되는 검증/변환되는 사용자 정의 함수인 clean_필드명(), clean() 함수 정의

--- askcompany/instagram/forms/form_PostForm.py
+++ askcompany/instagram/forms/form_PostForm.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 # . ⇐ 현재 폴더
 # .. ⇐ 현재 폴더의 부모 폴더
@@ -16,8 +18,21 @@ class PostForm(forms.ModelForm):
         # Post 모델에서 사용자가 선택한 특정 필드를 ModelForm 으로 사용
         # 필수 입력 필드가 문제 될 수 있음.
         fields = ['message', 'photo', 'tag_set', 'is_public']
+
         # 현재 상태로 저장(veiws.post_new 호출) 시, Post.author 이 필수 입력인데...없으므로
         # post = form.save() 함수 호출시 오류 발생....
         # 이에 form.save() 에 commit=False 인자를 추가하고
         # post.author 정보를 지정하고
         # post.save() 를 호출하여 저장
+
+        # 개별 field 에 대한 유효성 검증 로직을 추가 하려면 def clean_필드명() 함수를 추가로 정의
+        # 이는 form.is_valid() 할때 자동 호출 됨
+        def clean_message(self):
+            pass
+
+        def clean_photo(self):
+            pass
+
+        # 이러한 검증은 개별 필드에 대한 점검인데... n개 필드를 같이 점검 하려고 할 경우엔?
+        def clean(self):
+            pass
