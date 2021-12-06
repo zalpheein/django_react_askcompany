e6e97dd3661e4dfef3ec9fab2fb6aab16ce2ef83
xalphee <zalphee@gmail.com>
2021-12-06 14:55:45 +0900

    FBV 프로필 방식을.... CBV 프로필 방식으로 변경 예제

--- askcompany/accounts/views.py
+++ askcompany/accounts/views.py
@@ -1,4 +1,29 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
+
+# FBV 방식
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+
+# CBV 방식
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile.html'
+
+profile = ProfileView.as_view()
+
+
+
+
+
+
+
+
+
+
+
+
