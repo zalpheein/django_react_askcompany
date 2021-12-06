40c9b5bfce1837f71e4b082fe6c974c3ce5d2d52
xalphee <zalphee@gmail.com>
2021-12-06 14:47:54 +0900

    profile.html 생성 및 구현

--- askcompany/accounts/urls.py
+++ askcompany/accounts/urls.py
@@ -5,5 +5,6 @@ from .import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('profile/', views.profile, name='profile'),
 
 ]
